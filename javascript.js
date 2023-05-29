01a89056ae4508d5263cbf3543f7eb2fc4d41167
VeronicaVassallo <128144216+VeronicaVassallo@users.noreply.github.com>
2023-05-29 17:34:05 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+let numeroA = 12
+let numeroB = 20
+let sommaAddizione = numeroA + numeroB
+
+console.log('RISULTATO ADDIZIONE:',sommaAddizione)
+
+let x = 12
+
+let name = 'Veronica'
+
+let sommaSottrazione = 4 - x
+
+console.log('RISULTATO SOTTRAZIONE:',sommaSottrazione)
+
+let studenteEpicode = {
+    nome:'Veronica',
+    cognome: 'Vassallo',
+    hobby: 'informatica'
+}
+console.log(studenteEpicode)
+
+let name1 = 'john'
+let name2 = 'John'
+ 
+
+console.log('I due nomi sono diversi?:',name1 !== name2)
+
+
+console.log('I due nomi sono uguali se entrambi minuscoli?:',name1 === name2.toLowerCase())//perche vuole le parentesi tonde?
+
+
